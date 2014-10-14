c2ed7081affcf70f7095531f581cc8b16048e336
Salim Fadhley <sal@stodge.org>
2014-10-14 01:44:37 +0100

    Sorta, kinda able to do something almost useful.

--- src/pybkick/kick.py
+++ src/pybkick/kick.py
@@ -1,14 +1,73 @@
+import os
 import logging
-from pyboard import Pyboard
+from .pyboard import Pyboard
+import argparse
+
+log = logging.getLogger(__name__)
+
+
+# We do not modify these files
+NO_MODIFY = ['main.py', 'pybcdc.inf', 'README.txt', 'boot.py']
+
+class MissingSourceCode(RuntimeError):
+    """Raised when the source code the user is trying to kick does not exist.
+    """
+
+
+def kick(port, src, dst, entry_point, delay=1):
+    if not os.path.exists(src):
+        raise MissingSourceCode('%s does not exist' % src)
+    pb = Pyboard(port)
+    with pb.raw_repl():
+        for file_path in os.walk(src):
+            log.info("Kicking %s", file_path)
+            kick_single_file(pb, file_path, src, dst)
+            
+
+def kick_single_file(pb, file_path, src, dst):
+    statement = get_kick_statement(file_path, src, dst)
+    pb.exec(statement)
+    
+
+def get_kick_statement(file_path, src, dst):
+    """Generates a python statement that will re-create the specified file in dst on the pyboard
+    """
+    return "import pyb; pyb.LED(1).on()"
+
+
+def cmd_line_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--src', dest='src', help='source path')
+    parser.add_argument('--dst', dest='dst', help='remote destination path')
+    parser.add_argument('--entry_point', dest='entry_point', help='dotted path to the script that has to be run after kicking')
+    parser.add_argument('--port', dest='port', default='/dev/ttyACM0', help='Which serial port should we use')
+    return parser
+
 
 def main():
-    text = ["import pyb", "pyb.LED(1).on()"]
+    parser = cmd_line_parser()
+    opts = parser.parse_args()
     
-    pb = Pyboard('/dev/ttyACM0')
+    if not opts.src:
+        parser.print_help("--src must be defined.")
+        raise SystemExit()
     
-    with pb.raw_repl():
-        pb.exec("import pyb")
-        pb.exec("pyb.LED(4).on()")
+    return kick(
+         port = opts.port,
+         src = opts.src,
+         dst = opts.dst,
+         entry_point = opts.entry_point
+    )
+    
+    
+# def main():
+#     text = ["import pyb", "pyb.LED(1).on()"]
+#     
+#     pb = Pyboard('/dev/ttyACM0')
+#     
+#     with pb.raw_repl():
+#         pb.exec("import pyb")
+#         pb.exec("pyb.LED(1).on()")
         
 if __name__ == '__main__':
     logging.basicConfig()
---
+++ src/pybkick_tests/test_kick.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import mock
+import unittest
+import pkg_resources
+from pybkick.kick import kick, main as kick_main, MissingSourceCode
+
+class TestKick(unittest.TestCase):
+    """Test that we can kick code over to the PyBoard
+    """
+    
+    def testBasicKick(self):
+        test_data_path = pkg_resources.resource_filename(__name__, 'test_data')
+        
+        kick(
+             port='/dev/ttyACM0',
+             src=test_data_path,
+             dst='tmp',
+             entry_point=None
+        )
+        
+    def testKickFromCommandLine(self):
+        test_data_path = pkg_resources.resource_filename(__name__, 'test_data')
+        
+        fake_argv = [sys.argv[0], '--src=%s' % test_data_path, '--dst=tmp']
+        with mock.patch('sys.argv', fake_argv):
+            fake_kick = mock.Mock()
+            with mock.patch('pybkick.kick', fake_kick):
+                kick_main()
+                fake_kick.assert_called_once()
+                
+    def testKickMissingDirectory(self):
+        missing_test_data_path = os.path.join(pkg_resources.resource_filename(__name__, 'test_data'), 'missing')
+        
+        with self.assertRaises(MissingSourceCode):
+            kick(
+                 port='/dev/ttyACM0',
+                 src=missing_test_data_path,
+                 dst='tmp',
+                 entry_point=None
+            )
+                
+        
+       
+        
